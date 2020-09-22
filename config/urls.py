f89bbd62a034e045df416141453b7c055b102889
mena-hub <mssz.nnia@gmail.com>
2020-09-22 14:18:09 -0300

    Include existing URLconf module (pages/urls.py)

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path("users/", include("social.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
+    path('pages/', include('pages.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
