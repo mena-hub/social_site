0de948fcc083b6ec2fb998eab9edc5ce26772344
mena-hub <mssz.nnia@gmail.com>
2020-09-22 14:11:22 -0300

    Write our pages view

--- pages/views.py
+++ pages/views.py
@@ -1,1 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
+from .models import Page
+
+def pages(request):
+    pages = get_list_or_404(Page)
+    return render(request, "pages/page_list.html", {'pages': pages})
