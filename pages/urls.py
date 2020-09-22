dcd9622488ae20b8a957cc2ce705424e149e64aa
mena-hub <mssz.nnia@gmail.com>
2020-09-22 14:48:15 -0300

    Change pages view to class ListView

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from pages.views import PageListView
 from . import views
 
 urlpatterns = [
-    path('', views.pages, name="pages"),
+    path('', PageListView.as_view(), name='pages'),
     path('<int:page_id>/<slug:page_slug>/', views.page, name="page"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, get_list_or_404, get_object_or_404
+from django.views.generic.list import ListView
+from django.shortcuts import render, get_object_or_404
 from .models import Page
 
-def pages(request):
-    pages = get_list_or_404(Page)
-    return render(request, "pages/page_list.html", {'pages': pages})
+class PageListView(ListView):
+    model = Page
 
 def page(request, page_id, page_slug):
     page = get_object_or_404(Page, id=page_id)
