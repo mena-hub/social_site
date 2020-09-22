61168bf09b9a8bca200f69dd37e00456e8675586
mena-hub <mssz.nnia@gmail.com>
2020-09-22 12:55:37 -0300

    Refactor function-based view into class-based view

--- core/views.py
+++ core/views.py
@@ -1,7 +1,8 @@
+from django.views.generic.base import TemplateView
 from django.shortcuts import render
 
-def home(request):
-    return render(request, "core/home.html")
+class HomePageView(TemplateView):
+    template_name = "core/home.html"
 
 def sample(request):
     return render(request, "core/sample.html")
