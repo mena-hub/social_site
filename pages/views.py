4a82e7ca54ee77e0c2a1945862f30096886ea25c
mena-hub <mssz.nnia@gmail.com>
2020-09-22 14:33:27 -0300

    Define another view function >> page

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import Page
 
 def pages(request):
     pages = get_list_or_404(Page)
     return render(request, "pages/page_list.html", {'pages': pages})
+
+def page(request, page_id, page_slug):
+    page = get_object_or_404(Page, id=page_id)
+    return render(request, "pages/page_detail.html", {'page':page})
