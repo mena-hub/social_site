736e35dde771a756d1f26b5e23b8808f63718dd5
mena-hub <mssz.nnia@gmail.com>
2020-10-01 08:45:49 -0300

    Update mapping with delete URL pattern

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from pages.views import PageListView, PageDetailView, PageCreateView
+from pages.views import PageListView, PageDetailView, PageCreateView, PageDeleteView
 
 pages_patterns = ([
     path('', PageListView.as_view(), name='pages'),
     path('<int:pk>/<slug:slug>/', PageDetailView.as_view(), name="page"),
     path('create/', PageCreateView.as_view(), name="create"),
+    path('delete/<int:pk>', PageDeleteView.as_view(), name="delete"),
 ], 'pages')
