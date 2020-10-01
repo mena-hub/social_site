9e6a771ed1a514bc33197c0c218345dd6b9f9aa5
mena-hub <mssz.nnia@gmail.com>
2020-10-01 09:35:17 -0300

    Update mapping with update URL pattern

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from pages.views import PageListView, PageDetailView, PageCreateView, PageDeleteView
+from pages.views import PageListView, PageDetailView, PageCreateView, PageDeleteView, PageUpdateView
 
 pages_patterns = ([
     path('', PageListView.as_view(), name='pages'),
     path('<int:pk>/<slug:slug>/', PageDetailView.as_view(), name="page"),
     path('create/', PageCreateView.as_view(), name="create"),
-    path('delete/<int:pk>', PageDeleteView.as_view(), name="delete"),
+    path('delete/<int:pk>/', PageDeleteView.as_view(), name="delete"),
+    path('update/<int:pk>/', PageUpdateView.as_view(), name="update"),
 ], 'pages')
